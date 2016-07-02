6d59ea59b38e12304643d6ee369fcc4dd455b507
Bacra Woo <bacra.woo@gmail.com>
2016-07-03 04:28:21 +0800

    Add: parse 'W/X' repeat mark

--- src/dbfile_client.js
+++ src/dbfile_client.js
@@ -3,6 +3,9 @@ var MAX_GROUP_KEY_LENGTH = exports.MAX_GROUP_KEY_LENGTH = Math.pow(2, EACH_GROUP
 // 受到文件夹长度限制
 // http://stackoverflow.com/questions/14500893/is-the-255-char-limit-for-filenames-on-windows-and-unix-the-whole-path-or-part
 var MAX_GROUP_URI = exports.MAX_GROUP_URI = 250/MAX_GROUP_KEY_LENGTH | 0;
+var MAX_NOT_REPEAT_GROUP_MARK = 4;
+
+// console.log('DEFIND,%d,%d,%d,%d', EACH_GROUP_FILE_NUM, MAX_GROUP_KEY_LENGTH, MAX_GROUP_URI, MAX_NOT_REPEAT_GROUP_MARK);
 
 var OFFSET2INDEX = (function()
 	{
@@ -18,13 +21,25 @@ var OFFSET2INDEX = (function()
 	})();
 
 
+var MARK_Z_GROUPS = (function()
+	{
+		var arr = [];
+		var str = '';
+		for(var i = 1; i < MAX_GROUP_KEY_LENGTH; i++)
+		{
+			arr[i] = (str += 'Z');
+		}
+
+		return arr;
+	})();
+
 
 exports.key = key;
 // 除了32位的字符，转换后有如下特殊字符
 // Z  分组无任何数据，占位使用
 // Y  分组转成字符串之后，长度不足MAX_GROUP_KEY_LENGTH，补位
 // /  数据可能超过文件名长度限制，用来分割
-// X  当有很多Z的时候，为了美化，进行repeat处理
+// W...X  当有很多Z的时候，为了美化，进行repeat处理; ...表示重复的次数
 function key(indexs)
 {
 	var groups = [];
@@ -46,12 +61,10 @@ function key(indexs)
 	{
 		if (continuousEmptyGroups)
 		{
-			if (continuousEmptyGroups == 1)
-				str += 'Z';
-			else if (continuousEmptyGroups == 2)
-				str += 'ZZ';
+			if (MARK_Z_GROUPS[continuousEmptyGroups])
+				str += MARK_Z_GROUPS[continuousEmptyGroups];
 			else
-				str += continuousEmptyGroups+'X';
+				str += 'W'+continuousEmptyGroups+'X';
 
 			continuousEmptyGroups = 0;
 		}
--- test/test_dbfile.js
+++ test/test_dbfile.js
@@ -6,15 +6,38 @@ var ClientKey = require('../src/dbfile_client');
 
 describe('dbfile', function()
 {
-	doAssert('1', [0]);
-	doAssert('11', [0, 1]);
-	doAssert('101', [0, 2]);
-	doAssert('00101', [0, 2]);
-	doAssert('1000000011', [0, 1, 9]);
-	// doAssert('10000000111', [0, 8, 9]);
-	doAssert('00000001111', [0, 1, 2, 3]);
-	doAssert2('1Y1Y1', [0, 31, 62]);
-	doAssert2('1ZY1Y1', [0, 31, 93]);
+	assertParse1('1', [1]);
+	assertParse1('Y1', [1]);
+	assertParse1('YY1', [1]);
+	assertParse1('Z1', [1]);
+	assertParse1('ZZ1', [1]);
+	assertParse1('ZZZZZZZ1', [1]);
+	assertParse1('1Z1', [1,1]);
+	assertParse1('1ZZ1', [1, , 1]);
+	assertParse1('1Y1', [1,1]);
+	assertParse1('1YY1', [1, , 1]);
+	assertParse1('1W4X1', [1, , , , , 1]);
+	assertParse1('W4X1', [1]);
+
+	assertParse2('4X1', '4X');
+	assertParse2('4X', '4X');
+	assertParse2('WX1', 'WX');
+	assertParse2('1WX1', 'WX');
+	assertParse2('WCX1', 'WCX');
+	assertParse2('W1', 'W');
+	assertParse2('1W1', 'W');
+	assertParse2('1WTCX1', 'WTCX');
+
+
+	assertHandler1('1', [0]);
+	assertHandler1('11', [0, 1]);
+	assertHandler1('101', [0, 2]);
+	assertHandler1('00101', [0, 2]);
+	assertHandler1('1000000011', [0, 1, 9]);
+	assertHandler1('10000000111', [0, 1, 2, 10]);
+	assertHandler1('00000001111', [0, 1, 2, 3]);
+	assertHandler2('1Y1Y1', [0, 31, 62]);
+	assertHandler2('1ZY1Y1', [0, 31, 93]);
 
 
 	it('overflow#1ZZY1Y1', function()
@@ -42,13 +65,17 @@ describe('dbfile', function()
 	it('clientkey', function()
 	{
 		assert.equal(ClientKey.key([0]), 'Y1');
+		assert.equal(ClientKey.key([1]), 'Y2');
+		assert.equal(ClientKey.key([0,1]), 'Y3');
 		assert.equal(ClientKey.key([0, 30]), '1000001');
 		assert.equal(ClientKey.key([0, 31]), 'Y1Y1');
-		assert.equal(ClientKey.key([1000]), 'Y8032X');
+		assert.equal(ClientKey.key([1000]), 'Y80W32X');
+		assert.equal(ClientKey.key([2000]), 'Y2000W29X/W35X');
+		assert.equal(ClientKey.key([5000]), 'Yg0W21X/W35X/W35X/W35X/W35X');
 	});
-	doAssert3([0]);
-	doAssert3([0, 31]);
-	doAssert3([0, 31, 92, 93, 94]);
+	assertClientKey([0]);
+	assertClientKey([0, 31]);
+	assertClientKey([0, 31, 92, 93, 94]);
 
 
 	it('clientkey#long', function()
@@ -89,7 +116,37 @@ describe('dbfile', function()
 });
 
 
-function doAssert(use, list)
+function assertParse1(key, list)
+{
+	it('parse#'+key, function()
+	{
+		var db = new DBFile();
+		return db.parseFileArray(key)
+			.then(function(filekey)
+			{
+				assert.deepEqual(filekey, list);
+			});
+	});
+}
+
+function assertParse2(key, key2)
+{
+	it('parse#'+key, function()
+	{
+		var db = new DBFile();
+		return db.parseFileArray(key)
+			.then(function()
+			{
+				assert(false);
+			},
+			function(err)
+			{
+				assert.deepEqual(err.message, 'INVALID REPEAT MARK W/X,'+key2);
+			});
+	});
+}
+
+function assertHandler1(use, list)
 {
 	it('use#'+use, function()
 	{
@@ -105,7 +162,7 @@ function doAssert(use, list)
 	});
 }
 
-function doAssert2(use, list)
+function assertHandler2(use, list)
 {
 	it('use#'+use, function()
 	{
@@ -121,7 +178,7 @@ function doAssert2(use, list)
 	});
 }
 
-function doAssert3(use)
+function assertClientKey(use)
 {
 	it('clearkey#'+use.join(), function()
 	{
