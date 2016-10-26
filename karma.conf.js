720a73a94e9542c7869ec834893c28a733933816
Bacra <bacra.woo@gmail.com>
2016-10-26 21:36:50 +0800

    Add: test e2e

---
+++ karma.conf.js
@@ -0,0 +1,80 @@
+// Karma configuration
+// Generated on Wed Oct 26 2016 20:41:55 GMT+0800 (中国标准时间)
+
+var os = require('os');
+
+module.exports = function(config) {
+
+  var browsers = ['Chrome', 'Firefox'];
+  var platform = os.platform();
+
+  if (platform == 'win32') browsers.push('IE');
+  else if (platform == 'darwin') browsers.push('Safari');
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'browserify'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/test_clientkey.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+        'test/test_clientkey.js': ['browserify']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    // browsers: ['Chrome'],
+    browsers: browsers,
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
