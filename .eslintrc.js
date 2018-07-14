3402029045148269112118d72e9401790dcf0d63
Bacra Woo <bacra.woo@gmail.com>
2018-07-14 22:03:13 +0800

    Update: pkg version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports =
 {
-	env: {node: true},
-	extends: 'eslint:recommended',
-	rules:
-	{
-		'no-console': 'off',
-		'no-redeclare': 'off',
-	},
-}
+	extends: 'eslint-config-brcjs',
+};
